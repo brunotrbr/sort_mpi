c030b49d87ec0f230dd204f993e0b5a7705fbca0
Bruno Bragança Mendes <bbmendes@gmail.com>
2019-11-10 16:25:33 -0300

    usa heapsort apos mpi

--- bubblesort.c
+++ bubblesort.c
@@ -4,7 +4,82 @@
 #include <time.h>
 #include "utils.h"
 
-void bubble_sort (int vetor[], int n) {
+// A heap has current size and array of elements
+struct MaxHeap
+{
+    int size;
+    int* array;
+};
+
+// A utility function to swap to integers
+void swap(int* a, int* b) { int t = *a; *a = *b;  *b = t; }
+
+// The main function to heapify a Max Heap. The function
+// assumes that everything under given root (element at
+// index idx) is already heapified
+void maxHeapify(struct MaxHeap* maxHeap, int idx)
+{
+    int largest = idx;  // Initialize largest as root
+    int left = (idx << 1) + 1;  // left = 2*idx + 1
+    int right = (idx + 1) << 1; // right = 2*idx + 2
+
+    // See if left child of root exists and is greater than
+    // root
+    if (left < maxHeap->size &&
+        maxHeap->array[left] > maxHeap->array[largest])
+        largest = left;
+
+    // See if right child of root exists and is greater than
+    // the largest so far
+    if (right < maxHeap->size &&
+        maxHeap->array[right] > maxHeap->array[largest])
+        largest = right;
+
+    // Change root, if needed
+    if (largest != idx)
+    {
+        swap(&maxHeap->array[largest], &maxHeap->array[idx]);
+        maxHeapify(maxHeap, largest);
+    }
+}
+
+// A utility function to create a max heap of given capacity
+struct MaxHeap* createAndBuildHeap(int *array, int size)
+{
+    int i;
+    struct MaxHeap* maxHeap = (struct MaxHeap*) malloc(sizeof(struct MaxHeap));
+    maxHeap->size = size;   // initialize size of heap
+    maxHeap->array = array; // Assign address of first element of array
+
+    // Start from bottommost and rightmost internal mode and heapify all
+    // internal modes in bottom up way
+    for (i = (maxHeap->size - 2) / 2; i >= 0; --i)
+        maxHeapify(maxHeap, i);
+    return maxHeap;
+}
+
+// The main function to sort an array of given size
+void heapSort(int* array, int size)
+{
+    // Build a heap from the input data.
+    struct MaxHeap* maxHeap = createAndBuildHeap(array, size);
+
+    // Repeat following steps while heap size is greater than 1.
+    // The last element in max heap will be the minimum element
+    while (maxHeap->size > 1)
+    {
+        // The largest item in Heap is stored at the root. Replace
+        // it with the last item of the heap followed by reducing the
+        // size of heap by 1.
+        swap(&maxHeap->array[0], &maxHeap->array[maxHeap->size - 1]);
+        --maxHeap->size;  // Reduce heap size
+
+        // Finally, heapify the root of tree.
+        maxHeapify(maxHeap, 0);
+    }
+}
+
+void bubbleSort (int vetor[], int n) {
     int k, j, aux;
 
     for (k = 1; k < n; k++) {
@@ -18,11 +93,15 @@ void bubble_sort (int vetor[], int n) {
     }
 }
 
+void ordenaAposMpi(int dst[], int tamanhoDst){
+    heapSort(dst,tamanhoDst);
+}
+
 void inicializaVetor(int vetor[], int tamanhoVetor){
     srand(time(NULL));
     int i = 0;
     for(i = 0 ; i < tamanhoVetor ; i++){
-        vetor[i] = rand() % (tamanhoVetor * 10);
+        vetor[i] = rand() % (tamanhoVetor * 100);
     }
 }
 
@@ -32,6 +111,48 @@ void imprimeVetor(int vetor[], int tamanhoVetor){
         printf("%d ",vetor[i]);
     }
     printf("\n\n");
+}
+
+void imprimeArquivo(int vetor[], int tamanhoVetor, char tipo, int imprimeSaida)
+{
+    if(imprimeSaida == 1){
+        if(tipo == 'o'){
+            FILE * arquivo;
+            arquivo = fopen("saida.txt", "w");
+            int i;
+
+            fprintf(arquivo, "%s\n\t","Vetor original:");
+            for(i = 0; i < tamanhoVetor ; i++){
+                fprintf(arquivo,"%d ",vetor[i]);
+            }
+            fprintf(arquivo,"\n\n");
+
+            fclose(arquivo);
+        } else if(tipo == 'n'){
+            FILE * arquivo;
+            arquivo = fopen("saida.txt", "a+");
+            int i;
+
+            fprintf(arquivo, "%s\n\t","Vetor final nao ordenado:");
+            for(i = 0; i < tamanhoVetor ; i++){
+                fprintf(arquivo,"%d ",vetor[i]);
+            }
+            fprintf(arquivo,"\n\n");
+
+            fclose(arquivo);
+        } else {
+            FILE * arquivo;
+            arquivo = fopen("saida.txt", "a+");
+            int i;
+            fprintf(arquivo, "%s\n\t","Vetor final ordenado:");
+            for(i = 0; i < tamanhoVetor ; i++){
+                fprintf(arquivo,"%d ",vetor[i]);
+            }
+            fprintf(arquivo,"\n\n");
+            printf("\n\n");
+            fclose(arquivo);
+        }
+    }
 }
 
 /**
@@ -56,6 +177,7 @@ void juntaVetores(int src[], int tamanhoSrc, int dst[], int offset){
 
 int main(int argc, char *argv[])
 {
+    int imprimeSaida = atoi(argv[2]);
     int tamanhoVetor = atoi(argv[1]);
     int ret;
     char *hostname;
@@ -99,10 +221,11 @@ int main(int argc, char *argv[])
     int tamUltPos = tamVetSplit + resTamVetSplit;
     int *ultPosVetSpl = (int *)malloc(tamUltPos * sizeof(int));
     if(my_Rank == 0){
-        printf("Tamanho do vetor: %d\n", tamanhoVetor);
-        printf("Tamanho n vetores: %d | tamanho do ultimo vetor: %d\n\n", tamVetSplit, tamUltPos);
-        printf("Vetor original:\t");
-        imprimeVetor(vetor, tamanhoVetor);
+        if(imprimeSaida == 1){
+            printf("Tamanho do vetor: %d\n", tamanhoVetor);
+            printf("Tamanho n vetores: %d | tamanho do ultimo vetor: %d\n\n", tamVetSplit, tamUltPos);
+        }
+        imprimeArquivo(vetor,tamanhoVetor, 'o', imprimeSaida);
         // Realiza o envio para os processadores
         for(rank = 1 ; rank < proc_size; rank++){
             if(rank == proc_size-1){
@@ -131,7 +254,6 @@ int main(int argc, char *argv[])
                 if(ret != MPI_SUCCESS){
                     mpi_err(1,"MPI_Recv");
                 }
-                imprimeVetor(ultPosVetSpl, tamUltPos);
                 juntaVetores(ultPosVetSpl, tamUltPos, vetorFinal, offset);
             } else {
                 // demais processadores
@@ -139,19 +261,18 @@ int main(int argc, char *argv[])
                 if(ret != MPI_SUCCESS){
                     mpi_err(1,"MPI_Recv");
                 }
-                imprimeVetor(nVetSpl, tamVetSplit);
                 juntaVetores(nVetSpl, tamVetSplit, vetorFinal, offset);
                 offset += tamVetSplit;
             }
             
         } 
-        printf("proc_size: %d\n", proc_size);
-        printf("Vetor final nao ordenado:\t");
-        imprimeVetor(vetorFinal,tamanhoVetor);
-        sort_after_mpi(vetorFinal,tamanhoVetor,tamVetSplit,proc_size);
-        printf("Vetor ordenado:\t");
-        imprimeVetor(vetorFinal,tamanhoVetor);
-        printf("acabou\n");
+        imprimeArquivo(vetorFinal,tamanhoVetor, 'n', imprimeSaida);
+        ordenaAposMpi(vetorFinal, tamanhoVetor);
+        //sort_after_mpi(vetorFinal,tamanhoVetor,tamVetSplit,proc_size);
+        if(imprimeSaida == 1){
+            printf("Vetor ordenado impresso em saida.txt\t");
+        }
+        imprimeArquivo(vetorFinal,tamanhoVetor, 'r', imprimeSaida);
     } else {
         ret = MPI_Comm_rank(MPI_COMM_WORLD, &my_Rank);
         if(ret != MPI_SUCCESS){
@@ -164,7 +285,7 @@ int main(int argc, char *argv[])
             if(ret != MPI_SUCCESS){
                 mpi_err(1,"MPI_Recv");
             }
-            bubble_sort(ultPosVetSpl, tamUltPos);
+            bubbleSort(ultPosVetSpl, tamUltPos);
             
             ret = MPI_Send(ultPosVetSpl, tamUltPos * sizeof(int), MPI_INT, 0, 0, MPI_COMM_WORLD);
             if(ret != MPI_SUCCESS){
@@ -177,7 +298,7 @@ int main(int argc, char *argv[])
             if(ret != MPI_SUCCESS){
                 mpi_err(1,"MPI_Recv");
             }
-            bubble_sort(nVetSpl, tamVetSplit);
+            bubbleSort(nVetSpl, tamVetSplit);
             
             ret = MPI_Send(nVetSpl, tamVetSplit * sizeof(int), MPI_INT, 0, 0, MPI_COMM_WORLD);
             if(ret != MPI_SUCCESS){
